9b3b4028d2718a06f1a2d7a51d158f841356ae2c
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-01-22 14:55:54 +0100

    Music Media Player

--- mediaplayer.js
+++ mediaplayer.js
@@ -20,5 +20,6 @@ loadSong(songs[songIndex]);
 // Update song details
 function loadSong(song){
     title.innerText = song
-    audio.src
+    audio.src = `music/${song}.mp3`
+    cover.src = `images/${song}.jpg`
 }
