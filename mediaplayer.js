ac71b88fc1213faecc25c649805226d9224252d3
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-01-22 15:10:05 +0100

    Music Media Player

--- mediaplayer.js
+++ mediaplayer.js
@@ -9,7 +9,7 @@ const title = document.querySelector("#title")
 const cover = document.querySelector("#cover")
 
 // song Title //
-const songs =["anime",  "DinoRainbowGirlMusic"]
+const songs =["anime", "DansElectro", "DinoRainbowGirlMusic"]
 
 // Keep Track of songs //
 let songIndex = 1
