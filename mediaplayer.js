f0f45a519ee4fcd83132f40f8a50c19227edda8b
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-01-28 16:43:52 +0100

    Music Media Player

--- mediaplayer.js
+++ mediaplayer.js
@@ -21,19 +21,26 @@ loadSong(songs[songIndex])
 // Update song details //
 function loadSong(song){
     title.innerText = song
-    audio.src = 'music/${song}.mp3'
-    cover.src = 'images/${song}.jpg'
+    audio.src = 'music\${song}.mp3'
+    cover.src = 'images\${song}.jpg'
 }
 */
 function playSong() {
     musicContainer.classList.add("play")
     playBtn.querySelector("i.fas").classList.remove("fa-play")
     playBtn.querySelector("i.fas").classList.add("fa-pause")
+
+    audio.play()
 }
 
 function pauseSong() {
-    
+    musicContainer.classList.remove("play")
+    playBtn.querySelector("i.fas").classList.add("fa-play")
+    playBtn.querySelector("i.fas").classList.remove("fa-pause")
+
+    audio.pause()
 }
+
  //Event listener //
  playBtn.addEventListener("click", () => {
     const isPlaying = musicContainer.classList.contains("play")
