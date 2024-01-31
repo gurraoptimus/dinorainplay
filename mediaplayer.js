67e3607aa5254ced199cb13a41e03ccad9a04c17
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-01-31 21:07:40 +0100

    Music Media Player

--- mediaplayer.js
+++ mediaplayer.js
@@ -1,8 +1,7 @@
-
-const musicContainer = document.querySelector(".music-container")
-const playBtn = document.querySelector("#play")
-const prevBtn = document.querySelector("#prev")
-const nexBtn = document.querySelector("#next")
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 
 const audio = document.querySelector("#audio")
 const progress = document.querySelector(".progress")
@@ -24,14 +23,14 @@ loadSong(songs[songIndex])
 // Update song details //
 function loadSong(song){
     title.innerText = song
-    audio.src = `music/${song}.mp3`
-    cover.src = `images/${song}.jpg`
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpg`;
 }
 
 function playSong() {
     musicContainer.classList.add("play")
-    playBtn.querySelector("i.fas").classList.remove("fa-play")
-    playBtn.querySelector("i.fas").classList.add("fa-pause")
+    playBtn.querySelector("i.fas").classList.remove("fa-play");
+    playBtn.querySelector("i.fas").classList.add("fa-pause");
 
     audio.play();
 }
